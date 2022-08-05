aa4dda882c8082a9cc56e35aff1c665e2fc4fdcb
Peter Chen <peterchen2017125@gmail.com>
2022-08-05 16:34:45 +0800

    20220805

--- dist/index.7a353f27ca8300bbd707.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";console.log("here")})();
-//# sourceMappingURL=index.7a353f27ca8300bbd707.js.map
---
+++ dist/index.fde52ceffa8a200dd15c.js
@@ -0,0 +1,2 @@
+!function(){"use strict";console.log("here")}();
+//# sourceMappingURL=index.fde52ceffa8a200dd15c.js.map
