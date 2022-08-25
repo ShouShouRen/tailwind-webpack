508ab93236a6fbc04b5aca2b0da979376e473463
Peter Chen <peterchen2017125@gmail.com>
2022-08-25 22:57:30 +0800

    20220825

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-// import $ from 'jquery'
 import './index.css'
-import { a } from './a'
-
-// console.log($.ajax)
-
-console.log( a ,'...');
-
-class Test {
-    #a = 100
-}
-
-const tt = new Test()
-
-console.log('tt',tt.a);
